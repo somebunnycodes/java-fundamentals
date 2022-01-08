b8758230dc9a48f8b6083958ddd58a8f7f406f48
Bunny Mueller <thebunnymueller@gmail.com>
2022-01-08 20:59:14 +0100

    first commit

---
+++ FirstJavaProgram.java
@@ -0,0 +1,7 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("Bunny");
+        System.out.println("Age 3700");
+        System.out.println("Hometown: the Moon");
+    }
+}
